0cf57e1e9d5c1180ae0e1a49a27819f22f958137
Bobjohnlee <bintokbino@gmail.com>
2021-11-06 13:30:31 +0000

    project file

---
+++ arBookTest/Assets/script/imageRecognitionExample.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+public class imageRecognitionExample : MonoBehaviour
+{
+    
+    private ARTrackedImageManager ARTrackedImageManager;
+    private void Awake()
+    {
+        ARTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+    public void OnEnable()
+    {
+        ARTrackedImageManager.trackedImagesChanged += OnimageChanged;
+    } 
+    public void OnDisable()
+    {
+        ARTrackedImageManager.trackedImagesChanged -= OnimageChanged;
+    }
+
+    public void OnimageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
